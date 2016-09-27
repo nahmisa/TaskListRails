53d5e8101931de52be5387d3dec1d1c1015c71b9
Sarah Nahmias <nahmisa@gmail.com>
2016-09-27 16:16:17 -0700

    Creates list of tasks on root route
    Uses 'fake database' variable @tasks in tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,20 @@
 class TasksController < ApplicationController
   def index
+    @tasks = [
+      {
+        id: 1,
+        name: "Take out trash",
+        description: "Trash outside",
+        complete: true,
+      },
+      {
+        id: 2,
+        name: "Walk the dog",
+        description: "Dog outside",
+        complete: false,
+      },
+    ]
+
   end
 
   def show
