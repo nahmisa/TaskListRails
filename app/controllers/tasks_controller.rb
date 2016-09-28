fbae313ed40de236a87593f069b3cbd6bafe2d29
Sarah Nahmias <nahmisa@gmail.com>
2016-09-28 14:03:09 -0700

    Refactors out 'fake database'
    Links index & show views and controller actions with newly minted Task model and database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,40 +1,9 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [
-      {
-        id: 1,
-        name: "Take out trash",
-        description: "Trash outside",
-        complete: true,
-      },
-      {
-        id: 2,
-        name: "Walk the dog",
-        description: "Dog outside",
-        complete: false,
-      },
-    ]
-
+    @tasks = Task.all
   end
 
   def show
-    @tasks = [
-      {
-        id: 1,
-        name: "Take out trash",
-        description: "Trash outside",
-        complete: true,
-      },
-      {
-        id: 2,
-        name: "Walk the dog",
-        description: "Dog outside",
-        complete: false,
-      },
-    ]
-
-    @current_task = @tasks[params[:id].to_i - 1]
-    # IDs start at 1; the -1 to index the array
-    
+    @current_task = Task.find(params[:id])
   end
 end
