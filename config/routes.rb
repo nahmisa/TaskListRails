dc33b6da4627461944bb1ac06378a98b2e2ca198
Sarah Nahmias <nahmisa@gmail.com>
2016-09-29 15:36:27 -0700

    Updates spacing on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root   'tasks#index'
 
-  get 'tasks/'         => 'tasks#index'
-  post 'tasks/'        => 'tasks#create'
-  get 'tasks/new'      => 'tasks#new'
-  get 'tasks/:id/edit' => 'tasks#edit', as: "edit_task"
-  get 'tasks/:id'      => 'tasks#show', as: "task"
+  get    'tasks/'         => 'tasks#index'
+  post   'tasks/'         => 'tasks#create'
+  get    'tasks/new'      => 'tasks#new'
+  get    'tasks/:id/edit' => 'tasks#edit', as: "edit_task"
+  get    'tasks/:id'      => 'tasks#show', as: "task"
+  patch  'tasks/:id'      =>  'tasks#update'
 
 
-  delete 'tasks/:id'   => 'tasks#destroy'
+  delete 'tasks/:id'      => 'tasks#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
