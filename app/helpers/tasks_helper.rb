3ce60d24a1fbabf58a0227951917e1e9f5b9e766
Sarah Nahmias <nahmisa@gmail.com>
2016-09-30 15:12:31 -0700

    Refactors logic to complete/uncomplete to task helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,9 @@
 module TasksHelper
   def name_completed_class(task)
-    if task.completed_at != nil
-      "done"
-    else
-      "pending"
-    end
+    task.completed_at != nil ? "done" : "pending"
+  end
+
+  def make_completed_path_type(task)
+    task.completed_at != nil ? uncomplete_task_path(task) : complete_task_path(task)
   end
 end
