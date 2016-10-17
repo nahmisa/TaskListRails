c569ef0acbce0c5cf9072561a1f30a34d1aa3f6d
Sarah Nahmias <nahmisa@gmail.com>
2016-10-17 14:46:08 -0700

    Gets rid of sessions#create raise and builds a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,19 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
-      raise
+    redirect to login_failure_path unless auth_hash['uid']
+
+    @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+    if @user.nil?
+      # User doesn't match anything in the DB.
+      # Attempt to create a new user.
+      @user = User.build_from_github(auth_hash)
+      render :creation_failure unless @user.save
+    end
+
+    # Save the user ID in the session
+    session[:user_id] = @user.id
+
+    redirect_to root_path
   end
 end
