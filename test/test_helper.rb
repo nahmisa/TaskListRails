f1802fbbf8785948028fca3b495ec1084cf6aac3
Sarah Nahmias <nahmisa@gmail.com>
2016-10-21 15:19:17 -0700

    Adds tests to user model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,14 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def setup
+  # Turn on test mode - auth requests will be short-circuited
+    OmniAuth.config.test_mode = true
+  # What data should we get back from auth requests?
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+                                      provider: 'github',
+                                      uid: '99999',
+                                      info: { email: "a@b.com", name: "Ada" }
+                                      })
+  end
 end
